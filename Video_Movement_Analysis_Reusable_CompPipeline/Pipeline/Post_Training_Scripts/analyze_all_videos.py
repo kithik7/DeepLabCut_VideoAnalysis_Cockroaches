2f1aa0780bdfdf71f59549cf26e93ff117a6502f
Keerthi Kesavan <keerthikesavan06@gmail.com>
2026-05-20 15:42:15 +0200

    Update analyze_all_videos.py

--- Video_Movement_Analysis_Reusable_CompPipeline/Pipeline/Post_Training_Scripts/analyze_all_videos.py
+++ Video_Movement_Analysis_Reusable_CompPipeline/Pipeline/Post_Training_Scripts/analyze_all_videos.py
@@ -15,16 +15,16 @@ video_paths = [
     "/rhomes/kkesavan/dlc_project_group/August_2025_Group/Basler acA2040-90um (24827095)_20250820_161513132.mp4"
 ]
 
-print("Starting video analysis...")
+print("Starting video analysis..")
 dlc.analyze_videos(config_path, video_paths, save_as_csv=True)
 print("Video analysis complete!")
 
 print("Creating labeled videos...")
 dlc.create_labeled_video(config_path, video_paths, draw_skeleton=True)
-print("Labeled videos created!")
+print("Labelled videos processed")
 
 print("Plotting trajectories...")
 dlc.plot_trajectories(config_path, video_paths)
-print("Trajectory plots created!")
+print("Trajectory plots generated")
 
-print("🎉 ALL PROCESSING COMPLETE!")
+print("ALL PROCESSING COMPLETE!")
